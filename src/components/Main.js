c678723964b2fbd48442b0b294f4db78f3dde71a
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-12-05 10:48:41 -0600

    added some madeUp classes 4 position in after and before

--- src/components/Main.js
+++ src/components/Main.js
@@ -30,6 +30,14 @@ const Main = () => {
             <h2 className='text-4xl font-bold text-center sm:text-5xl mb-6 text-slate-900 dark:text-white'>
                 Testimonials
             </h2>
+            <figure className='my-12'>
+                <blockquote className='bg-teal-600 dark:bg-black pl-14 pr-8 py-12 rounded-3xl relative'>
+                    <p className='text-2xl sm:text-3xl text-left mt-2 text-white dark:text-slate-400 custom-quote custom-quote-font custom-quote-position custom-quote_ custom-quote-font_ custom-quote-position_' ></p>
+                </blockquote>
+            </figure>
+            <figcaption className='italic text-xl sm:text-2xl text-right mt-2 text-slate-500 dark:text-slate-400' >
+                &#8212; Jesus V. Arias, Genius
+            </figcaption>
         </section>
         <hr className='mx-auto bg-black dark:bg-white w-1/2' />
         <section id='contact' className='p-6 my-12'>
