62165236585ab6e0ae777000f50dc46b56773778
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-12-09 10:42:53 -0600

    addded a user reducer to the store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './slices/counterSlice'
 import postReducer from './slices/postSlice'
+import userReducer from './slices/userSlice'
 
 
 export default configureStore({
   reducer: {
     counterReducer,
     postReducer,
+    userReducer,
   },
 })
